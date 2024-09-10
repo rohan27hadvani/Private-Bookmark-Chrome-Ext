2c59c02d28dc0d71a1d65641de96066f6207241c
rohan27hadvani <rohan.hadvani0@gmail.com>
2024-09-10 17:14:19 +0530

    2nd Commit

--- index.js
+++ index.js
@@ -5,10 +5,14 @@ const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
-    
+    inputEl.value=""
+    renderLeads()
 })
 
-for (let i = 0; i < myLeads.length; i++) {
-    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
-    
-}
+function renderLeads() {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += "<li>" + myLeads[i] + "</li>"
+    }
+    ulEl.innerHTML = listItems
+} 
