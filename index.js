2b172f1d9006152df790551c3704ad31fde44e1a
rohan27hadvani <rohan.hadvani0@gmail.com>
2024-09-11 10:37:23 +0530

    Final Testign Done

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ if (leadsFromLocalStorage) {
 }
 
 tabBtn.addEventListener("click", function() {
-    chrome.tabs.query{active: true, currentWindow: true, function(tabs) {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         myLeads.push(tabs[0].url)
         localStorage.setItem("myLeads", JSON.stringify(myLeads))
         render(myLeads)
     }
 
-    }
+)
 
   
 })
